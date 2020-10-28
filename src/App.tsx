01b7f62f9a930b87a113dac793f8a8036153018a
Lauren Clark <lauren.c4231@gmail.com>
2020-10-28 13:37:50 +0000

    Add Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Header from './components/Header'
-import Layout from './layouts/main'
+import Layout from './layouts/Main'
+
+import LaunchImg from './assets/img/launch-home@3x.png'
 
 import './App.scss'
 
 function App() {
   return (
+    <>
+      <Header />
     <Layout>
       <Header />
     </Layout>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,8 +1,17 @@
 import React from 'react'
+import Logo from '../assets/img/spacex-logo.png'
 import './Header.module.scss'
 
 function Header() {
-  return <header>Hello world!</header>
+  return (
+    <header>
+      <span>
+        <img src={Logo} alt="SpaceX Launces Logo" />
+        Launches
+      </span>
+      <button>Reload Data</button>
+    </header>
+  )
 }
 
 export default Header
