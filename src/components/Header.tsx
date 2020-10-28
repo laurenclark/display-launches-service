ea0b2279cc5304eef55f32dc116ea994eb4f26e6
Lauren Clark <lauren.c4231@gmail.com>
2020-10-28 18:54:17 +0000

    Images changes - add icons - add srcsets - swap html images for Image component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Logo from '../assets/img/spacex-logo.png'
+import Image from 'react-simple-image'
 import Button from './Button'
 import './Header.scss'
 
@@ -7,11 +7,26 @@ function Header() {
   return (
     <header>
       <span>
-        <img src={Logo} alt="SpaceX Launces Logo" />
+        <Image
+          alt="SpaceX Launches Logo"
+          src="./img/spacex-logo.png"
+          srcSet={{
+            '3x': './img/spacex-logo.png'
+          }}
+        />
         Launches
       </span>
       <Button radius="25px 0 0 25px" padding="0.7rem 1rem">
         Reload Data
+        <Image
+          alt="Refresh Icon"
+          src="./icon/refresh.png"
+          className="icon icon__refresh"
+          srcSet={{
+            '3x': './icon/refresh@3x.png',
+            '2x': './icon/refresh@2x.png'
+          }}
+        />
       </Button>
     </header>
   )
