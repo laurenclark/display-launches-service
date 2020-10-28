ab1d4fb060f5caedbfdc7481c0d8a7cf7604590e
Lauren Clark <lauren.c4231@gmail.com>
2020-10-28 16:16:06 +0000

    Dummy table row styles

---
+++ src/components/TableRow.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function TableRow() {
+  return (
+    <div className="table-row">
+      <div className="table-row__number">#1</div>
+      <div className="table-row__title">FalconSat</div>
+      <div className="table-row__details">
+        <time>24th March 2006</time>
+        <p>Falcon 1</p>
+      </div>
+    </div>
+  )
+}
+
+export default TableRow
