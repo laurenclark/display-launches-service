c8d5064bd4766c39fdcc7912e8d3785691215874
Lauren Clark <lauren.c4231@gmail.com>
2020-10-28 20:37:02 +0000

    Add load spinner

---
+++ src/components/LoadingSpinner.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function LoadingSpinner({ text = 'Loading...' }: { text: string }) {
+  return (
+    <div className="loading" aria-placeholder="Loading">
+      <h3>{text}</h3>
+      <div role="status" className="loading__ripple">
+        <div role="presentation"></div>
+        <div role="presentation"></div>
+      </div>
+    </div>
+  )
+}
+
+export default LoadingSpinner
