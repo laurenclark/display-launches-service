a837cb371cffd70e4c768da6d26c73c1cab38bf1
Lauren Clark <laurenclark@Laurens-Mac.local>
2020-10-28 18:54:17 +0000

    Images changes - add icons - add srcsets - swap html images for Image component

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import React from 'react'
 import Header from './components/Header'
 import Layout from './layouts/Main'
 import TableRow from './components/TableRow'
-import LaunchImg from './assets/img/launch-home@3x.png'
 import Button from './components/Button'
+import Image from 'react-simple-image'
 import './App.scss'
 
 function App() {
@@ -12,11 +12,40 @@ function App() {
       <Header />
       <Layout>
         <div className="container">
-          <img src={LaunchImg} alt="A spaceship launching from a launch pad" />
+          <Image
+            alt="Space ship launching"
+            src="./img/launch-home.png"
+            srcSet={{
+              '2x': './img/launch-home@2x.png',
+              '3x': './img/launch-home@3x.png'
+            }}
+          />
           <div>
             <div className="button-container">
-              <Button padding="0.8rem 1rem">Filter by Year</Button>
-              <Button padding="0.8rem 1rem">Sort Descending</Button>
+              <Button padding="0.8rem 1rem">
+                Filter by Year
+                <Image
+                  alt="Select Icon"
+                  src="./icon/select.png"
+                  className="icon icon__select"
+                  srcSet={{
+                    '3x': './icon/select@3x.png',
+                    '2x': './icon/select@2x.png'
+                  }}
+                />
+              </Button>
+              <Button padding="0.8rem 1rem">
+                Sort Descending
+                <Image
+                  alt="Sort Icon"
+                  src="./icon/sort.png"
+                  className="icon icon__sort"
+                  srcSet={{
+                    '3x': './icon/sort@3x.png',
+                    '2x': './icon/sort@2x.png'
+                  }}
+                />
+              </Button>
             </div>
             <TableRow
               index={1}
