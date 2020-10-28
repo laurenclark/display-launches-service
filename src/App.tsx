58343401609553d0c8556eb93d9ab8a20d5ec460
Lauren Clark <lauren.c4231@gmail.com>
2020-10-28 16:39:19 +0000

    Dummy props

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import Header from './components/Header'
 import Layout from './layouts/Main'
-
+import TableRow from './components/TableRow'
 import LaunchImg from './assets/img/launch-home@3x.png'
-
+import Button from './components/Button'
 import './App.scss'
 
 function App() {
@@ -13,6 +13,18 @@ function App() {
       <Layout>
         <div className="container">
           <img src={LaunchImg} alt="A spaceship launching from a launch pad" />
+          <div>
+            <div className="button-container">
+              <Button padding="0.8rem 1rem">Filter by Year</Button>
+              <Button padding="0.8rem 1rem">Sort Descending</Button>
+            </div>
+            <TableRow
+              index={1}
+              title="FalconSat"
+              date={new Date().toDateString()}
+              ship="Falcon 1"
+            />
+          </div>
         </div>
       </Layout>
     </>
--- src/components/TableRow.tsx
+++ src/components/TableRow.tsx
@@ -1,13 +1,20 @@
 import React from 'react'
 
-function TableRow() {
+interface TableData {
+  index: number
+  title: string
+  date: string
+  ship: string
+}
+
+function TableRow({ index, title, date, ship }: TableData) {
   return (
     <div className="table-row">
-      <div className="table-row__number">#1</div>
-      <div className="table-row__title">FalconSat</div>
+      <div className="table-row__number">#{index}</div>
+      <div className="table-row__title">{title}</div>
       <div className="table-row__details">
-        <time>24th March 2006</time>
-        <p>Falcon 1</p>
+        <time>{date}</time>
+        <p>{ship}</p>
       </div>
     </div>
   )
