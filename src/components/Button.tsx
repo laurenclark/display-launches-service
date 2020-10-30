0bf69425a7ba0b77eb5862cf780fcf54942ec1af
Lauren Clark <lauren.c4231@gmail.com>
2020-10-30 14:42:36 +0000

    Type button and add clickhandler

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -2,17 +2,27 @@ import React from 'react'
 import './Button.scss'
 
 type Props = {
-  children: React.ReactNode
+  children?: React.ReactNode
   padding?: string
   radius?: string
+  clickHandler?: (e: React.MouseEvent<HTMLButtonElement>) => void
 }
 
-function Button({ padding, radius, children }: Props) {
+function Button({
+  padding,
+  radius,
+  children,
+  clickHandler
+}: React.PropsWithChildren<Props>) {
   const ButtonStyles = {
     padding: `${padding}`,
     borderRadius: `${radius}`
   }
-  return <button style={ButtonStyles}>{children}</button>
+  return (
+    <button onClick={clickHandler} style={ButtonStyles}>
+      {children}
+    </button>
+  )
 }
 
 export default Button
